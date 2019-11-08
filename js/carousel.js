8235b24bab8ef96863b506373389d3065bc2ee29
krystek77 <krystian.wrona77@gmail.com>
2019-11-08 10:25:30 +0100

    add function that hide the correct button

--- js/carousel.js
+++ js/carousel.js
@@ -47,16 +47,37 @@ function toggleActive(currentSlide, targetSlide) {
 	targetSlide.classList.add('active');
 }
 
+/**
+ * Hide the correct button
+ *
+ * @param {HTMLElement} targetSlide
+ * @param {HTMLElement} slides
+ */
+function hideButton(targetSlide, slides) {
+	if (targetSlide === slides[0]) {
+		nextButton.classList.remove('hide');
+		prevButton.classList.add('hide');
+	} else if (targetSlide === slides[slides.length - 1]) {
+		nextButton.classList.add('hide');
+		prevButton.classList.remove('hide');
+	} else {
+		nextButton.classList.remove('hide');
+		prevButton.classList.remove('hide');
+	}
+}
+
 positionSlides(slides, slideWidth);
 
 nextButton.addEventListener('click', function() {
 	const currentSlide = carousel.querySelector('.active');
 	const targetSlide = currentSlide.nextElementSibling;
+	hideButton(targetSlide, slides);
 	moveSlide(carousel, currentSlide, targetSlide);
 });
 
 prevButton.addEventListener('click', function() {
 	const currentSlide = carousel.querySelector('.active');
 	const targetSlide = currentSlide.previousElementSibling;
+	hideButton(targetSlide, slides);
 	moveSlide(carousel, currentSlide, targetSlide);
 });
